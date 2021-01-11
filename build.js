1ab28fc7ea2fe40ed2cef88b79a41c12e35657e1
LuckRain7 <763839938@qq.com>
2021-01-11 10:06:31 +0800

    build

--- build.js
+++ build.js
@@ -3,12 +3,12 @@ const MdToHtmlPlugin = require("./md-to-html-plugin/index.js");
 MdToHtmlPlugin([
   {
     entryFilename: "bookmarks.md",
-    template: "bookmarks",
+    template: "template.v2",
     outFilename: "index.html",
   },
   {
     entryFilename: "github.md",
-    template: "github",
+    template: "template.v2",
     outFilename: "github.html",
   },
 ]);
